e0d56d21927476799b917894a6e75da309b03e53
Prabhdeep Singh <prabhd33ps@gmail.com>
2021-03-06 16:07:27 +0000

    QUestion_Code_Till_partB.1

---
+++ Assignment1/Code/Q1_code.py
@@ -0,0 +1,57 @@
+from pyspark.sql import SparkSession
+import pyspark.sql.functions as F
+from pyspark.sql.functions import udf
+from pyspark.sql import types as t
+import matplotlib.pyplot as plt
+
+
+def extract_website(s):
+    s1 = s.split(".")
+    return s[s.index(s1[-3]):]
+
+spark = SparkSession.builder \
+    .master("local[2]") \
+    .appName("COM6021_Question_1") \
+    .config("spark.local.dir","/fastdata/lip20ps") \
+    .getOrCreate()
+
+sc = spark.sparkContext
+sc.setLogLevel("WARN")  # This can only affect the log level after it is executed.
+
+logFile = spark.read.text("/Users/pskaloya/Documents/TUOS/2-Semester/COM6012ScalableMachineLearning/ScalableML-Prabh-GIT/Assignment1/Data/NASA_access_log_Jul95.gz").cache()
+
+host_data = logFile.withColumn('host', F.regexp_extract('value', '^(.*) - -.*', 1)).drop("value").cache()
+
+
+hosts_Japan_uni = host_data.filter(host_data.host.endswith(".ac.jp")).count()
+hosts_UK_uni = host_data.filter(host_data.host.endswith(".ac.uk")).count()
+hosts_US_uni = host_data.filter(host_data.host.endswith(".edu")).count()
+
+print("\n\nThere are %i hosts from Japan Universities.\n\n" % (hostsJapan_uni))
+print("\n\nThere are %i hosts from UK Universities.\n\n" % (hostsUK_uni))
+print("\n\nThere are %i hosts from US Universities.\n\n" % (hostsUS_uni))
+
+
+fig, ax = plt.subplots(1,1, figsize =(8,6))
+ax.bar(["Japan","UK","US"],[hosts_Japan_uni,hosts_UK_uni,hosts_US_uni])
+ax.set_xlabel('Count')
+ax.set_ylabel('Countries')
+plt.savefig("/Users/pskaloya/Documents/TUOS/2-Semester/COM6012ScalableMachineLearning/ScalableML-Prabh-GIT/Assignment1/Output/Question1_A.png")
+
+
+
+extract_website_udf = udf(extract_website, t.StringType())
+
+hosts_Japan_uni_extracted = hosts_Japan_uni.withColumn('host',extract_website_udf('host')).groupBy("host").orderBy('count',ascending=False).limit(9).cache()
+
+hostnames_Japan_top9 = hosts_Japan_uni_extracted.select('host').collect()
+hostnames_Japan_count_top9 = hosts_Japan_uni_extracted.select('count').collect()
+
+fig, ax = plt.subplots(1,1, figsize =(8,6))
+ax.pie(x=hostnames_Japan_count_top9,labels=hostnames_Japan_top9,radius=2,textprops = {'fontsize':10, 'color':'black'},autopct = '%3.2f%%')
+ax.set_xlabel('Count')
+ax.set_ylabel('Countries')
+plt.savefig("/Users/pskaloya/Documents/TUOS/2-Semester/COM6012ScalableMachineLearning/ScalableML-Prabh-GIT/Assignment1/Output/Question1_B.png")
+
+
+spark.stop()
